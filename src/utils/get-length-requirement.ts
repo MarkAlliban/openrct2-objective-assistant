813f4b0cb22399d59f8a3f05991edda5567691db
MarkAlliban <115544029+MarkAlliban@users.noreply.github.com>
2026-03-17 16:29:43 +0000

    Document bug related to objective.length
    Added a link to the bug report for clarity.

--- src/utils/get-length-requirement.ts
+++ src/utils/get-length-requirement.ts
@@ -1,7 +1,8 @@
 export const getLengthRequirement = (objective: ScenarioObjective): number | null => {
   if (objective.length) return objective.length;
 
-  // BUG: objective.length is declared, but not provided at time of coding.
+  // BUG: https://github.com/OpenRCT2/OpenRCT2/issues/26200
+  // objective.length is declared, but not provided at time of coding.
   // If it's fixed in a future release of OpenRCT2 then we will pick it up here.
   // Until then, there are only 4 official scenarios that use this objective:
   if (scenario.name === "Octagon Park") return 1200;
